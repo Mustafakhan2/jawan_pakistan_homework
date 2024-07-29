ac12671978d865d5ed7895762d5c991b98ab6740
Mustafa Khan <97780351+Mustafakhan2@users.noreply.github.com>
2024-07-28 20:40:01 -0700

    fixed the 3 question

--- assignment_5/queston3.dart
+++ assignment_5/queston3.dart
@@ -1,7 +1,11 @@
 //Q. 3)  create a list  of name, class, roll no, grade, percentage. And print. 
 void main(){
 List students = ['Mustafa','1 year', '423254','A','85'];
-print(students);
+print(students[0]);
+print(students[1]);
+print(students[2]);
+print(students[3]);
+print(students[4]);
 }
 
 

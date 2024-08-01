5fb9dd40a8d7b8963ef422c10e2169a455e4ac5d
Mustafa Khan <97780351+Mustafakhan2@users.noreply.github.com>
2024-08-01 08:19:01 -0700

    done assignment no 7
    write about 10 method of list

---
+++ assignment_7/add_all.dart
@@ -0,0 +1,7 @@
+void main()
+{
+  List<int> numbers = [1, 2, 3];
+numbers.addAll([4, 5, 6]);
+print(numbers);
+
+}
---
+++ assignment_7/remove_at.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<int> numbers = [1, 2, 3];
+  int removed = numbers.removeAt(1);
+  print(numbers);
+  print(removed);
+}
